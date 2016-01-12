257011709341bd1364974fa3dd0165178dbac950
Andre Souffrant <andresouffrant@gmail.com>
2016-01-11 22:50:46 -0500

    connected to twitter stream

--- twitter/twitter_stream.py
+++ twitter/twitter_stream.py
@@ -1,14 +1,32 @@
 import tweepy
+from tweepy.streaming import StreamListener
 from configobj import ConfigObj
-import os
 
 # define variables
 twitter_conf_filename = "../config/twitter.conf"
 
 def readConfig(filename):
+    ''' reads the config file into a dictionary object '''
     return ConfigObj(filename)
 
-if __name__ == "__main__":
-    config = readConfig(twitter_conf_filename)
-    print(config["consumer_key"])
+class StdOutListener(StreamListener):
+    ''' sets up a listener that prints data and errors '''
+    def on_data(self, data):
+        print(data)
+        return True
 
+    def on_error(self, status):
+        print(status)
+
+if __name__ == "__main__":
+    # read config
+    config = readConfig(twitter_conf_filename)
+    
+    # authenticates and sets up stream
+    listener = StdOutListener()
+    auth = tweepy.OAuthHandler(config["consumer_key"], config["consumer_secret"])
+    auth.set_access_token(config["access_token"], config["access_token_secret"])
+    stream = tweepy.Stream(auth, listener)
+
+    # filter the stream
+    stream.filter(track=["AAPL"])
